2e253ffe361dd037942ac840ec0ac6743e237e6e
maijuriina <maiju.mustonen@gmail.com>
2019-05-17 09:29:53 +0300

    Deleted code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Komponentin muuttujaotsikko on tässä!';
-  message = '';
-
-  onButtonClick() {
-    console.log('Moikka!');
-    this.message = 'Moro!';
-  }
 }
