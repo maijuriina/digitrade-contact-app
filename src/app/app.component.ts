df3a9abe465ae27d50814cbe8c5ddd9d94bf078c
maijuriina <maiju.mustonen@gmail.com>
2019-05-17 11:50:13 +0300

    .app changed in four places to prefix .dtca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dtca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Kontaktisovellus digitrade 2019';
 }
