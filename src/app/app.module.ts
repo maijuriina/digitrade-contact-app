2af8bff42fe7f4c369d471a000891b14e359c684
maijuriina <maiju.mustonen@gmail.com>
2019-05-17 12:04:53 +0300

    Contact-list-item component added and .spec deleted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule,
